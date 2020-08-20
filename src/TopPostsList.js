f8178c2ead00a18cdb9d5d6a0a4e75987274c918
diana <diliang4783573@gmail.com>
2020-08-20 11:50:11 -0700

    added starter component for TopPostsList

---
+++ src/TopPostsList.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+/**
+ * Display the top 5 trending posts 
+ * 
+ * Ordered by number of up votes
+ */
+const TopPostsList = () => {
+  return (
+    <div className="TopPostsList">
+      <ul className="TopPostsList-list">
+        <li>first oneeee</li>
+      </ul>
+    </div>
+  );
+}
